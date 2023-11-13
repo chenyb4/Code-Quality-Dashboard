de5ebc737dc614acfb18ff80975771a0dbc4692d
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-11-13 09:24:25 +0100

    red and green up-down arrows to show increase and decrease of the last 30 days

--- src/app/components/DashBoard.tsx
+++ src/app/components/DashBoard.tsx
@@ -5,37 +5,18 @@ import {getDataByDaysAgo} from "@/app/utils/dataRangePicker";
 import {MyCard} from "@/app/components/MyCard";
 
 
-function getImprovement(historyArray: { date: string, value: string }[]): number {
-    const firstItem = historyArray[0];
-    const lastItem = historyArray[historyArray.length - 1];
-    const improvement = parseInt(lastItem.value) - parseInt(firstItem.value);
-    return improvement;
-
-}
-
-
-
 // @ts-ignore
 export async function Dashboard() {
 
     const data = await getDataByDaysAgo(30);
 
-
     console.log(data);
-
-    // let improvementsArray = [];
-    //
-    // data.measures.forEach((item: { metric: string, history: { date: string, value: string }[] }) => {
-    //     const improvement = getImprovement(item.history);
-    //     improvementsArray.push(improvement);
-    // });
-
 
     return (
             <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
-                <MyCard title="Code Coverage" history={data.measures[0].history}/>
-                <MyCard title="Cognitive Complexity" history={data.measures[1].history}/>
-                <MyCard title="Technical Debt" history={data.measures[2].history}/>
+                <MyCard title="Code Coverage" history={data.measures[0].history} isIncreasePositive={true}/>
+                <MyCard title="Cognitive Complexity" history={data.measures[1].history} isIncreasePositive={false}/>
+                <MyCard title="Technical Debt" history={data.measures[2].history} isIncreasePositive={false}/>
             </Grid>
     );
 }
--- src/app/components/MyCard.tsx
+++ src/app/components/MyCard.tsx
@@ -5,6 +5,8 @@ import React from "react";
 interface Props {
     title:string;
     history: { date: string, value: string }[];
+    isIncreasePositive:boolean;
+
 }
 
 function getCurrentValue(historyArray: { date: string, value: string }[]){
@@ -12,23 +14,40 @@ function getCurrentValue(historyArray: { date: string, value: string }[]){
     return lastItem.value;
 }
 
-export function MyCard({title,history}:Props){
+function getImprovement(historyArray: { date: string, value: string }[]): number {
+    const firstItem = historyArray[0];
+    const lastItem = historyArray[historyArray.length - 1];
+    const improvement = parseInt(lastItem.value) - parseInt(firstItem.value);
+    return improvement;
 
+}
 
+export function MyCard({title,history,isIncreasePositive}:Props){
+
+    let badgeDelta = null;
+    //last value - first value
+    let improvement=getImprovement(history);
+    let improvementAbsolute=Math.abs(improvement);
+    if(improvement>=0){
+        badgeDelta=<BadgeDelta deltaType="moderateIncrease" isIncreasePositive={isIncreasePositive}>{improvementAbsolute}</BadgeDelta>;
+    }else{
+        badgeDelta=<BadgeDelta deltaType="moderateDecrease" isIncreasePositive={isIncreasePositive}>{improvementAbsolute}</BadgeDelta>;
+    }
 
     return(
         <Card className='w-96 shadow-lg border-gray-300 border' >
 
-            <Text className="text-3xl font-bold dark:text-white text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-emerald-400 ">{title}</Text>
-            <BadgeDelta ></BadgeDelta>
-            <Metric className="text-emerald-600 mt-3 border-2 w-fit rounded-2xl border-emerald-600 p-2">{getCurrentValue(history)}</Metric>
+            <Text className="text-3xl font-bold dark:text-white text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-emerald-400 ">
+                {title}
+            </Text>
 
+            {badgeDelta}
 
-
-
+            <Metric className="text-emerald-600 mt-3 border-2 w-fit rounded-2xl border-emerald-600 p-2">
+                {getCurrentValue(history)}
+            </Metric>
 
             <MyLineChart data={history}/>
-
 
         </Card>
     )
