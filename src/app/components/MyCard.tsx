c1d003615bf1a438bcea3467be7d3dd2988ae63c
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-11-13 14:13:58 +0100

    some logics to show tech debt in housr and minutes and not just total number of minutes

--- src/app/components/MyCard.tsx
+++ src/app/components/MyCard.tsx
@@ -16,14 +16,46 @@ export function MyCard({title,history,isIncreasePositive}:Props){
         persentageMark="%";
     }
 
+    //current value for tech debt
+    let minutes=null;
+    let hours=null;
+    if(title=="Technical Debt"){
+        hours=Math.floor(parseInt(getCurrentValue(history)) / 60);
+        minutes=parseInt(getCurrentValue(history))% 60;
+    }
+
     let badgeDelta = null;
     //last value - first value
     let improvement=getImprovement(history);
     let improvementAbsolute=Math.abs(improvement);
-    if(improvement>=0){
-        badgeDelta=<BadgeDelta className="float-left ml-4" deltaType="moderateIncrease" isIncreasePositive={isIncreasePositive}>{improvementAbsolute}{persentageMark}</BadgeDelta>;
+
+    let metric=null;
+
+    if(title=="Technical Debt"){
+        let currentMinutesInTotal=parseInt(getCurrentValue(history));
+        hours=Math.floor(currentMinutesInTotal / 60);
+        minutes=currentMinutesInTotal% 60;
+        metric= <Metric className="text-emerald-600 mt-12 border-2 w-fit rounded-2xl border-emerald-600 p-2">{hours}h {minutes}m</Metric>
     }else{
-        badgeDelta=<BadgeDelta className="float-left ml-4" deltaType="moderateDecrease" isIncreasePositive={isIncreasePositive}>{improvementAbsolute}{persentageMark}</BadgeDelta>;
+        metric=<Metric className="text-emerald-600 mt-12 border-2 w-fit rounded-2xl border-emerald-600 p-2">{getCurrentValue(history)}{persentageMark}</Metric>
+    }
+
+    if(improvement>=0){
+        if(title=="Technical Debt"){
+            hours=Math.floor(improvement / 60);
+            minutes=improvement% 60;
+            badgeDelta=<BadgeDelta className="float-left ml-4" deltaType="moderateIncrease" isIncreasePositive={isIncreasePositive}>{hours}h {minutes}m</BadgeDelta>;
+        }else{
+            badgeDelta=<BadgeDelta className="float-left ml-4" deltaType="moderateIncrease" isIncreasePositive={isIncreasePositive}>{improvementAbsolute}{persentageMark}</BadgeDelta>;
+        }
+    }else{
+        if(title=="Technical Debt"){
+            hours=Math.floor(improvementAbsolute / 60);
+            minutes=improvementAbsolute% 60;
+            badgeDelta=<BadgeDelta className="float-left ml-4" deltaType="moderateDecrease" isIncreasePositive={isIncreasePositive}>{hours}h {minutes}m</BadgeDelta>;
+        }else{
+            badgeDelta=<BadgeDelta className="float-left ml-4" deltaType="moderateDecrease" isIncreasePositive={isIncreasePositive}>{improvementAbsolute}{persentageMark}</BadgeDelta>;
+        }
     }
 
 
@@ -33,9 +65,7 @@ export function MyCard({title,history,isIncreasePositive}:Props){
                 {title}
             </Text>
             {badgeDelta}
-            <Metric className="text-emerald-600 mt-12 border-2 w-fit rounded-2xl border-emerald-600 p-2">
-                {getCurrentValue(history)}{persentageMark}
-            </Metric>
+            {metric}
             <MyLineChart historyArray={history}/>
         </Card>
     )
