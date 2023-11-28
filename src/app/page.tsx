da864135d56162813136d02315cea79290092480
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-11-28 03:02:34 +0100

    padding of select dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home({searchParams}:Props) {
     let projects = projectsData.components;
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-10">
             <div>
                 <MySelect teams={projects} defaultValue='launchpad'/>
 
