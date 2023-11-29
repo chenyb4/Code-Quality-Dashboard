b9fd3691e62197e3e9a8a8668d303d3605c844b8
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-11-29 22:42:34 +0100

    process the entire measures array in data object to convert date string to  Date type and value string to number   type

--- src/app/components/DashBoard.tsx
+++ src/app/components/DashBoard.tsx
@@ -9,12 +9,40 @@ interface Props {
     component: string;
 }
 
+
 export async function Dashboard({component}: Props) {
 
     const data = await getDataByDaysAgo(30, component);
 
+    let measures: { metric: string, history: { date: Date, value: number }[] }[] = [];
+
+    // this is for converting the data into Date type and value into number type in all the data points in the data object's measures array
+    // every measure in the measures array is an object with metric member of type string and history member of type array
+    // @ts-ignore
+    data.measures.forEach((measure) => {
+        //push every converted measure to the data variable
+        let tempMeasure: { metric: string, history: { date: Date, value: number }[] };
+        tempMeasure = {metric: measure.metric, history: []};
+        // every dp in the history array is an object with data and value members
+        measure.history.forEach((dp: { date: string, value: string }) => {
+            let tempDp: { date: Date, value: number };
+            tempDp = {date: new Date(dp.date), value: parseInt(dp.value)};
+            tempMeasure.history.push(tempDp);
+        });
+        measures.push(tempMeasure)
+    });
+
+
+    // measures.forEach((measure)=>{
+    //     measure.history.forEach((dp)=>{
+    //         console.log(typeof dp.value);
+    //         console.log(typeof dp.date);
+    //     })
+    // })
+
+
     //rules is a string, multiple rules can be divided by comma
-    let deprecationData = await getSonarQubeIssuesByRules(component, '500', 'typescript:S1874','OPEN,CONFIRMED,REOPENED,CLOSED');
+    let deprecationData = await getSonarQubeIssuesByRules(component, '500', 'typescript:S1874', 'OPEN,CONFIRMED,REOPENED,CLOSED');
 
     let numberOfDeprecationTS = deprecationData.issues.length;
 
