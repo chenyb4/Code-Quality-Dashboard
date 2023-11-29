c30d7a1f3932cc5285f9f4d4fce9b4c831ea5a04
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-11-29 23:50:24 +0100

    create config file for metrics. refactor code to make use of metric config file.

---
+++ src/app/config/metricsConfig.ts
@@ -0,0 +1,36 @@
+
+export const metrics={
+    "codeCoverage":{
+        title:"Code Coverage",
+        key:"coverage",
+        isIncreasePositive:true,
+        formatToPercentage:true,
+        formatToHoursAndMinutes:false,
+    },
+    "cognitiveComplexity":{
+        title:"Cognitive Complexity",
+        key:"cognitive_complexity",
+        isIncreasePositive:false,
+        formatToPercentage:false,
+        formatToHoursAndMinutes:false,
+    },
+    "technicalDebt":{
+        title:"Technical Debt",
+        key:"sqale_index",
+        isIncreasePositive:false,
+        formatToPercentage:false,
+        formatToHoursAndMinutes:true,
+    },
+    "numberOfDeprecations":{
+        title:"Number of Deprecations",
+        key:"",
+        isIncreasePositive:false,
+        formatToPercentage:false,
+        formatToHoursAndMinutes:false,
+    }
+}
+
+
+
+
+
