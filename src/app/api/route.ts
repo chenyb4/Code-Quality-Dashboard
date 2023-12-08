37d8e8f39a78760b6f4920701d7e8ee5f3208730
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-12-08 09:40:50 +0100

    database, api

---
+++ src/app/api/route.ts
@@ -0,0 +1,60 @@
+import sqlite3 from "sqlite3";
+import {open, Database} from "sqlite";
+import type {NextApiRequest, NextApiResponse} from "next";
+
+let db = null;
+
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+    if (!db) {
+        db = await open({
+            filename: "./src/app/db/db.db",
+            driver: sqlite3.Database,
+        });
+    }
+
+    const items = await db.all("SELECT * FROM history");
+
+    return new Response(JSON.stringify(items), {
+        headers: {"Content-Type": "application/json"},
+        status: 200,
+    });
+}
+
+
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+
+    try {
+        const body=await req.json();
+        const {projectKey, metricKey, date, value} =body;
+
+        if (!db) {
+            db = await open({
+                filename: "./src/app/db/db.db",
+                driver: sqlite3.Database,
+            });
+        }
+
+        const result = await db.run('INSERT INTO history(projectKey, metricKey, date, value) VALUES(?, ?,?,?)', [projectKey, metricKey, date, value], (err) => {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log('Row was added to the table: ${this.lastID}');
+        })
+
+        console.log(result);
+
+        return new Response(
+            JSON.stringify(
+                {message: "success"},
+                {
+                    headers: {"content-type": "application/json"},
+                    status: 200,
+                }
+            )
+        );
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+}
