1bc5f3d1e22f23f9a986f6183a91086e53b9d542
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-12-13 16:15:04 +0100

    api get request to get metrics history from DB

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,21 @@
 import sqlite3 from "sqlite3";
 import {open, Database} from "sqlite";
 import type {NextApiRequest, NextApiResponse} from "next";
+import {sq} from "date-fns/locale";
 
 let db = null;
 
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
+
+    const projectKey=req.nextUrl.searchParams.get(['projectKey']);
+    const metricKey=req.nextUrl.searchParams.get(['metricKey']);
+
+
+    let sql="SELECT * FROM history where ";
+    sql+=" projectKey='"+projectKey+"' and";
+    sql+=" metricKey='"+metricKey+"'";
+
+
     if (!db) {
         db = await open({
             filename: "./src/app/db/db.db",
@@ -12,7 +23,7 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
         });
     }
 
-    const items = await db.all("SELECT * FROM history");
+    const items = await db.all(sql);
 
     return new Response(JSON.stringify(items), {
         headers: {"Content-Type": "application/json"},
