8965574441f500efb2fbe4dcdacc11735ea1db2a
Yibing Chen <yibing.chen@drukwerkdeal.nl>
2023-12-13 17:24:11 +0100

    use enum formatting type in metric config  to configure the types of formatting for  the metric's current value

--- src/app/config/metricsConfig.ts
+++ src/app/config/metricsConfig.ts
@@ -1,32 +1,29 @@
 
+import {FormattingType} from "@/app/utils/FormattingType";
 export const metrics={
     "codeCoverage":{
         title:"Code Coverage",
         key:"coverage",
         isIncreasePositive:true,
-        formatToPercentage:true,
-        formatToHoursAndMinutes:false,
+        formatting:FormattingType.PERCENTAGE,
     },
     "cognitiveComplexity":{
         title:"Cognitive Complexity",
         key:"cognitive_complexity",
         isIncreasePositive:false,
-        formatToPercentage:false,
-        formatToHoursAndMinutes:false,
+        formatting:FormattingType.ABSOLUTE,
     },
     "technicalDebt":{
         title:"Technical Debt",
         key:"sqale_index",
         isIncreasePositive:false,
-        formatToPercentage:false,
-        formatToHoursAndMinutes:true,
+        formatting:FormattingType.HOURSANDMINUTES,
     },
     "numberOfDeprecations":{
         title:"Number of Deprecations",
         key:"deprecation",
         isIncreasePositive:false,
-        formatToPercentage:false,
-        formatToHoursAndMinutes:false,
+        formatting:FormattingType.ABSOLUTE,
     }
 }
 
